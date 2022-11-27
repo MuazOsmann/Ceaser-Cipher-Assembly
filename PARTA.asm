bfb084b3f686e2750d04a1d1fc2679c642457d6e
Muaz Osman <90202813+MuazOsmann@users.noreply.github.com>
2022-11-27 20:48:36 +0400

    Minor Fix :)

--- PARTA.asm
+++ PARTA.asm
@@ -23,6 +23,7 @@ O_WRONLY equ 000001q ; write only
 O_RDWR equ 000002q ; read and write
 S_IRUSR equ 00400q
 S_IWUSR equ 00200q
+
 section .data
   bufsize dw 1024   ; number of characters, each character has a word (2 bytes) woth of memory specified for it
   FileName db "msg.txt",0   ; name of the source file - the file that includes the text to be ciphered
